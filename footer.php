e99f4095a98c44839e5353477c61d0847aa3f846
seanrote24 <seanrote85@gmail.com>
2026-04-29 15:50:09 +0800

    Changes to be committed: 	modified:   contact.php 	modified:   footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+</div>
+<footer style="text-align: center; margin-top: 20px; color: #777;">
+    <?php
+    echo '<p>&copy; 2026 made by Sean Gabriel B. Rote. All rights reserved.</p>';
+    ?>
+</footer>
+</body>
+</html>
